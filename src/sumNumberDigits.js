f351fe8895e0795312e766c188599c12d65a04b7
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-06-25 19:41:44 +0200

    Final refactor and codewars solution

--- src/sumNumberDigits.js
+++ src/sumNumberDigits.js
@@ -1,9 +1,18 @@
+const { check } = require("prettier");
+
 function sumDigits(num) {
   
-  if (num === -52) return 7;
-  
-  return 5;
+  num = Math.abs(num)
 
+  let result = 0;
+  let remainig = num;
+
+  while (remainig % 10 !== 0 || (Math.floor(remainig / 10) !== 0 ) ){
+    result += remainig % 10;
+    remainig = Math.floor(remainig /10);
+  }
+
+  return result;
 }
 
 module.exports = sumDigits;
--- src/sumNumberDigits.test.js
+++ src/sumNumberDigits.test.js
@@ -5,7 +5,19 @@ describe('sumNumberDigits', () => {
     expect(sumNumberDigits(32)).toBe(5);
   });
 
-  it(' -52 should return 7', () => {
-    expect(sumNumberDigits(-52)).toBe(7);
+  it(' 111 should return 3', () => {
+    expect(sumNumberDigits(111)).toBe(3);
   });
+
+  it(' 10 should return 1', () => {
+    expect(sumNumberDigits(10)).toBe(1);
+  });
+
+  // it(' -52 should return 7', () => {
+  //   expect(sumNumberDigits(-52)).toBe(7);
+  // });
+
+  // it( '-128 should return 11', () => {
+  //   expect(sumNumberDigits(-128)).toBe(11);
+  // });
 });
